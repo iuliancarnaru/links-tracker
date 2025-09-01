ab27b95439b11549226ff0e1e263326608ce907c
Iulian Carnaru <iulian.carnaru@gmail.com>
2025-09-01 05:37:07 +0100

    add object storage

--- apps/data-service/src/workflows/destination-evaluation.ts
+++ apps/data-service/src/workflows/destination-evaluation.ts
@@ -1,9 +1,13 @@
 import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
 import { collectDestinationInfo } from '@/helpers/browser-render';
+import { addEvaluation } from '@repo/data-ops/queries/evalutations';
+import { initDatabase } from '@repo/data-ops/database';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
 	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+		initDatabase(this.env.DB);
+
 		const collectedData = await step.do('Collect rendered destination page data', async () => {
 			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
@@ -21,6 +25,22 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
 			}
 		);
 
-		console.log(collectedData);
+		const evaluationId = await step.do('Save evaluation in database', async () => {
+			return await addEvaluation({
+				linkId: event.payload.linkId,
+				status: aiStatus.status,
+				reason: aiStatus.statusReason,
+				accountId: event.payload.accountId,
+				destinationUrl: event.payload.destinationUrl,
+			});
+		});
+
+		await step.do('Backup destination HTML in R2', async () => {
+			const accountId = event.payload.accountId;
+			const r2PathHtml = `evaluations/${accountId}/html/${evaluationId}`;
+			const r2PathBodyText = `evaluations/${accountId}/body-text/${evaluationId}`;
+			await this.env.BUCKET.put(r2PathHtml, collectedData.html);
+			await this.env.BUCKET.put(r2PathBodyText, collectedData.bodyText);
+		});
 	}
 }
